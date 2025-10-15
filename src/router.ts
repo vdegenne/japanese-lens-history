18d690c6d9026cda16299a5f715938ced325495a
Valentin Degenne <vdegenne@gmail.com>
2025-10-15 18:18:24 +0200

    few improvs

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,11 @@
 import {Hash, Router} from '@vdegenne/router';
-import toast from 'toastit';
 import {store} from './store.js';
 
 export default new (class {
 	hash = new Hash<{search: string; page: Page}>();
 
-	#router = new Router(async ({}) => {
+	router = new Router(async ({}) => {
 		await store.updateComplete;
-		// toast(`${store.search} ${store.page}`);
 		if (this.hash.has('search')) {
 			const search = this.hash.$('search');
 			if (search !== store.search) {
